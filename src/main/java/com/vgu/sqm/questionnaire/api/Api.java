57e001d7aee564f2dfadb0f6eec468254b4e14f7
Noe <eminemempire3@gmail.com>
2022-01-23 01:37:09 +0700

    Upload project

---
+++ src/main/java/com/vgu/sqm/questionnaire/api/Api.java
@@ -0,0 +1,33 @@
+package com.vgu.sqm.questionnaire.api;
+
+import com.vgu.sqm.questionnaire.utils.StringUtils;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class Api extends HttpServlet {
+  /** throws HTTP 500 Internal server error */
+  protected static void handleHttp500Error(HttpServletResponse response, Exception e, Logger logger)
+      throws ServletException, IOException {
+    logger.log(Level.SEVERE, e.toString());
+    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    response.getWriter().print("Oh no, you broke the server! Call the admins!");
+  }
+
+  /** handles cases where one or more parameters is missing */
+  protected static void handleMissingParameters(HttpServletResponse response, String... params)
+      throws ServletException, IOException {
+    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    response.getWriter().print(StringUtils.messageMissingParameters(params));
+  }
+
+  /** handles cases where one or more parameters is invalid */
+  protected static void handleInvalidParameters(HttpServletResponse response, String... params)
+      throws ServletException, IOException {
+    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    response.getWriter().print(StringUtils.messageInvalidParameters(params));
+  }
+}
